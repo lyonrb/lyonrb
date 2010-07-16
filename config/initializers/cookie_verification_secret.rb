c840dbf4d6bfe4eb03849e7eccc6ab86f1c21ee3
Damien Mathieu <damien.mathieu@lim.eu>
2010-07-16 16:46:55 +0200

    better cookie token

--- config/initializers/cookie_verification_secret.rb
+++ config/initializers/cookie_verification_secret.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '610f8ae38a52aa9a13099d2195f727b258cb130dddb7a10bf00088a0acd6f4b4bc3446fa3277c697910f7ed1efe5b4eda4235d06184e082172655656a82ef99a'
+Rails.application.config.secret_token = '610f8ae38a52aa9a13099d2195f727b258cb130dddb7a10bf00088a0acd6f4b4bc3446fa3277c697910f7ed1efe5b4eda4235d06184e082172655656a82ef99a'
