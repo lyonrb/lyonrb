b30ff62699d05e322f081a408ad20556c336e734
Damien Mathieu <damien.mathieu@lim.eu>
2010-07-16 16:55:06 +0200

    avoid warning because of the session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,7 +4,7 @@
 # If you change this key, all old sessions will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
+Rails.application.config.session_store = {
   :key    => '_lyonrb_session',
   :secret => '8df25d173c2643f22b174fb7cb64185f599bfef912873b4e264a5be5d0fc39bc5b94b9fd5f3f82179aa089f8a296d164991dde4512a2aa48d81d186bf3d89b82'
 }
