3f603af21d917411dd9441348e7ac3edc6585b8b
Franck Verrot <franck@verrot.fr>
2010-08-16 07:36:48 +0200

    some tests would be nice now :D

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,16 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     @content_for_title = "Rubyist community of Lyon, France"
-    html = Net::HTTP.get(URI.parse("http://github.com/lyonrb"))
-    doc = Nokogiri::HTML(html)
-    @members = doc.search("ul.org-members li").map do |member|
-      avatar = member.search("img").attribute('src').text
-      login = member.search("h4 a").text
-      if member.search("h4 em").text =~ /\((.*)\)/
-        name = $1
-      end
-      repo_summary = member.search("p").text
-      { :login => login, :name => name, :repo_summary => repo_summary, :avatar => avatar }
-    end
+    @members = Lyonrb::GithubMember.all
+    @projects = Lyonrb::GithubProject.all
   end
 end
---
+++ app/models/lyonrb/github_member.rb
@@ -0,0 +1,28 @@
+module Lyonrb
+  class GithubMember
+    attr_accessor :name, :login, :avatar, :email
+
+    def initialize(attribs = {})
+      self.name = attribs.fetch(:name, nil)
+      self.login = attribs.fetch(:login, nil)
+      self.avatar = attribs.fetch(:avatar, nil)
+      self.email = attribs.fetch(:email, "lyonrb@lyonrb.fr")
+    end
+    
+    class << self
+      def all
+        html = Net::HTTP.get(URI.parse("http://github.com/lyonrb"))
+        doc = Nokogiri::HTML(html)
+        members = doc.search("ul.org-members li").map do |member|
+          avatar = member.search("img").attribute('src').text
+          login = member.search("h4 a").text
+          if member.search("h4 em").text =~ /\((.*)\)/
+            name = $1
+          end
+          repo_summary = member.search("p").text
+          Lyonrb::GithubMember.new :login => login, :name => name, :repo_summary => repo_summary, :avatar => avatar
+        end
+      end
+    end
+  end
+end
