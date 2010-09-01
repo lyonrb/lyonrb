39676fa07c41eb22a0131315a2525eebff7c5704
Damien Mathieu <damien.mathieu@lim.eu>
2010-09-01 14:11:23 +0200

    the welcome controller tests need to mock github

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -4,6 +4,10 @@ describe WelcomeController do
   include Devise::TestHelpers
   
   describe 'index' do
+    before :all do
+      HttpMocks.repositories_list
+      HttpMocks.members_list
+    end
     it 'should succeed' do
       get :index
       response.should be_success
