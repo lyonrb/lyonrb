9fc7baeacaedfa71e6fcb2d19196e9698c54f5e3
Franck Verrot <franck@verrot.fr>
2010-09-02 07:23:58 +0200

    backticks wont work on pg

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,6 @@ class Event < ActiveRecord::Base
   validates_presence_of :name, :when, :place_name, :address
 
   def self.next
-    self.where(['`when` >= ?', DateTime.now.to_s(:db)]).order('`when` ASC').first
+    self.where(["'when' >= ?", DateTime.now.to_s(:db)]).order("'when' ASC").first
   end
 end
