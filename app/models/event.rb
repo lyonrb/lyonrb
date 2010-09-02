5c175cb3ea66958b603c9d9d3a72eb1d9735bd32
Franck Verrot <franck@verrot.fr>
2010-09-02 07:10:54 +0200

    Added the address field to the events admin panel.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ActiveRecord::Base
   validates_presence_of :name, :when, :place_name, :address
-  
+
   def self.next
     self.where(['`when` >= ?', DateTime.now.to_s(:db)]).order('`when` ASC').first
   end
