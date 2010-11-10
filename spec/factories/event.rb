4ff57306d85cab551838b48c351cd7293cc9b11e
Camille Appert <bob@bibinou.com>
2010-11-11 00:24:38 +0100

    Spicing up the Event factory

--- spec/factories/event.rb
+++ spec/factories/event.rb
@@ -1,6 +1,6 @@
 Factory.define :event do |u|
   u.name { Factory.next(:name) }
   u.start_at   { 1.day.from_now }
-  u.address "Lyon, France"
+  u.address "6 rue du Boeuf, Lyon 5e arrondissement"
   u.place_name { Factory.next(:name) }
 end
