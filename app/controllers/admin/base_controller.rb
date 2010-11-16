6bea9b03669bbc091a8ad95a2e96b7efae50fb8a
Camille Appert <bob@bibinou.com>
2010-11-16 15:58:39 +0100

    use 403 Forbidden instead of 401 Unauthorized
    401 is for HTTP Basic Auth

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -2,7 +2,7 @@ class Admin::BaseController < ApplicationController
   before_filter :authorization
   
   rescue_from CanCan::AccessDenied do |exception|
-    render 'errors/401', :layout => 'application'
+   render 'errors/403', :layout => 'application', :status => 403
   end
   
   private
--- spec/controllers/admin/index_controller_spec.rb
+++ spec/controllers/admin/index_controller_spec.rb
@@ -2,14 +2,24 @@ require 'spec_helper'
 
 describe Admin::IndexController do
   include Devise::TestHelpers
-  before :each do
-    @user = member_login Factory(:admin)
-  end
-  
+
   describe 'index' do
-    it 'should succeed' do
-      get :index
-      response.should be_success
+    context "when not logged in" do
+      it 'should be forbidden' do
+        get :index
+        response.status.should == 403
+      end
+    end
+
+    context "when logged in as admin" do
+      before :each do
+        @user = member_login Factory(:admin)
+      end
+
+      it 'should succeed' do
+        get :index
+        response.should be_success
+      end
     end
   end
 end
